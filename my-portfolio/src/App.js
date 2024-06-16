e61268ae1babd5f35913f2b8916a81b7cf5dd159
Brandon <bwkong88@gmail.com>
2024-06-16 17:44:00 -0400

    changed background

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import About from "./components/About"; 
 import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 export default function App() {
   return (
-    <main className = "text-gray-400 bg-gray-900 body-font">
+    <main className = "text-gray-400 bg-black body-font">
       <Navbar />
       <About />
       <Projects />
