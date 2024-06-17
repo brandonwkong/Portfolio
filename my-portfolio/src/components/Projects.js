2ac494ac3e8f97b4a44a329e97d67c60c3517fed
Brandon <bwkong88@gmail.com>
2024-06-16 23:58:40 -0400

    test image

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -18,10 +18,10 @@ export default function Projects() {
               href={project.link}
               key={project.image}
               className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
+              <div className="flex relative overflow-hidden">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center transition-transform duration-300 transform scale-100 hover:scale-105 brightness-90 hover:brightness-20"
+                  className="absolute inset-0 w-full h-full object-cover object-center transform transition duration-500 ease-in-out hover:scale-105 hover:brightness-20"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
