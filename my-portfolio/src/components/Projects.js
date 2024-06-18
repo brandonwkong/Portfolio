242651a7c50fa0d81832356c81b41b153a421f86
Brandon <bwkong88@gmail.com>
2024-06-18 11:12:57 -0400

    outline

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -4,7 +4,7 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id="projects" className="text-gray-300 bg-gradient-to-r from-gray-900 via-gray-750 to-gray-600 body-font">
+    <section id="projects" className="text-gray-300 bg-gradient-to-r from-gray-1000 via-gray-850 to-gray-700 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white border border-gray-400 inline-block px-4 py-2">
@@ -22,7 +22,7 @@ export default function Projects() {
               <div className="flex relative overflow-hidden w-full h-full">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center transform transition duration-500 ease-in-out hover:scale-105 hover:brightness-75"
+                  className="absolute inset-0 w-full h-full object-cover object-center transform transition duration-500 ease-in-out"
                   src={project.image}
                   style={{ height: '100%' }} // Ensure image takes full height of the container
                 />
